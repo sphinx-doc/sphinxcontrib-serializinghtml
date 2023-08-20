5c62ad4219698d410426e99495203e9484d77f29
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2023-08-21 00:20:10 +0100

    Properly serialise css_files and script_files

--- sphinxcontrib/serializinghtml/__init__.py
+++ sphinxcontrib/serializinghtml/__init__.py
@@ -63,6 +63,11 @@ class SerializingHTMLBuilder(StandaloneHTMLBuilder):
         return docname + SEP
 
     def dump_context(self, context: dict, filename: str | os.PathLike[str]) -> None:
+        context = context.copy()
+        if 'css_files' in context:
+            context['css_files'] = [css.filename for css in context['css_files']]
+        if 'script_files' in context:
+            context['script_files'] = [js.filename for js in context['script_files']]
         if self.implementation_dumps_unicode:
             with open(filename, 'w', encoding='utf-8') as ft:
                 self.implementation.dump(context, ft, *self.additional_dump_args)
